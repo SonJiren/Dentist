abca658acdd7f877f5ea0db289f0704284756052
SonJiren <jirenthegreyuniver11>
2024-09-18 09:14:53 -0500

    Algo bien

--- routes/console.php
+++ routes/console.php
@@ -15,22 +15,22 @@ use App\Jobs\EnviarRecordatorioCita;
 
  Artisan::command('RecordatorioCita', function () {
     $now = Carbon::now();
+    $fiveMinutesLater = $now->copy()->addMinutes(5);
     $destinatarios = ['agitokanoh657@gmail.com'];
+
     $this->info('Fecha y hora actuales: ' . $now->toDateTimeString());
 
-    // Obtener citas que necesitan recordatorio (5 minutos antes de la hora de la cita)
+    // Obtener citas que necesitan recordatorio (entre ahora y 5 minutos después)
     $citas = Cita::whereDate('fecha', $now->toDateString())
-                 ->whereTime('hora', $now->copy()->addMinutes(5)->format('H:i'))
+                 ->whereBetween('hora', [$now->format('H:i'), $fiveMinutesLater->format('H:i')])
                  ->get();
 
-    // Enviar recordatorio solo si hay citas
     if ($citas->isNotEmpty()) {
         foreach ($citas as $cita) {
             Mail::bcc($destinatarios)->send(new CitaCreada($cita));
             $this->info("Recordatorio enviado para la cita con ID {$cita->id}.");
         }
     } else {
-
         $this->info('No hay citas pendientes para enviar recordatorio.');
     }
 })->purpose('Enviar recordatorio 5 minutos antes de la cita')->everyMinute();
